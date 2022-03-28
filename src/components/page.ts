1a6b24a1612090da253f46e0ef8700fc03594fe0
Gayun00 <gayunlee11@gmail.com>
2022-03-28 22:42:06 +0900

    Feat: 페이지 컴포넌트 추가

---
+++ src/components/page.ts
@@ -0,0 +1,12 @@
+export class PageComponent {
+	private element: HTMLUListElement;
+	constructor() {
+		this.element = document.createElement('ul');
+		this.element.setAttribute('class', 'page');
+		this.element.textContent = '페이지 컴포넌트';
+	}
+
+	attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+		parent.insertAdjacentElement(position, this.element);
+	}
+}
